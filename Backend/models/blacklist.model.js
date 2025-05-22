8bdb16a551f78eb41258769702685efe4b90803f
RanjeetRaj444 <ranjeetraj44666@gmail.com>
2025-05-22 09:31:13 +0530

    Updated UI

--- Backend/models/blacklist.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const blackListSchema = new mongoose.Schema({
-    blacklist : {type: [String]}
-})
-
-const BLackListModel = mongoose.model("blacklist", blackListSchema);
-
-module.exports = {BLackListModel};
